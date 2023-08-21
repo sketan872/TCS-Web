afe0abadba7d2849e5a4191a1fac4e8d6a96e7ee
Vidhu Kant Sharma <vidhukant@vidhukant.com>
2023-08-22 00:01:27 +0530

    added form

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -11,6 +11,7 @@ import {
 
 import CircularLogoAndText from "./../components/Sections/CircularLogoAndText";
 import CardsSection from "./../components/Sections/CardsSection";
+import FormSection from "./../components/Sections/FormSection";
 import SocietyGoalCard from "./../components/Cards/SocietyGoalCard";
 import ProjectsCard from "./../components/Cards/ProjectsCard";
 import TechnologyCard from "./../components/Cards/TechnologyCard";
@@ -121,6 +122,10 @@ export default function HomePage() {
               />
           )
         }
+        />
+
+      <FormSection
+        title={"Registration Form"}
         />
     </>
   );
