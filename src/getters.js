568af7c1cae19c5fb87e0d1a27a0a3ece76d7c0a
Rahul Goyal <rahulgoyalvinay666@gmail.com>
2023-12-27 21:17:12 +0530

    new comment

--- src/getters.js
+++ src/getters.js
@@ -7,25 +7,25 @@
 export function getPostHolders() {
   return [
     {
-      img: "https://tcs.vidhukant.xyz/images/madhur.jpg",
+      img: "https://images/madhur.jpg",
       imgAlt: "Madhur Verma (President)",
       goalName: "Madhur Verma",
       goalRole: "President",
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/mahima.jpg",
+      img: "https://images/mahima.jpg",
       imgAlt: "Mahima Bhardwaj (Vice President)",
       goalName: "Mahima Bhardwaj",
       goalRole: "Vice President",
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/mohit.jpg",
+      img: "https:///mohit.jpg",
       imgAlt: "Mohit Saini (Technical Head )",
       goalName: "Mohit Saini",
       goalRole: "Technical & Project Head",
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/vanshika.jpg",
+      img: "https://images/vanshika.jpg",
       imgAlt: "Vanshika Kakkar (Media Head)",
       goalName: "Vanshika Kakkar",
       goalRole: "Media & Creative Head",
@@ -36,35 +36,35 @@ export function getPostHolders() {
 export function getMentors() {
   return [
     {
-      img: "https://tcs.vidhukant.xyz/images/jyoti.jpg",
+      img: "https://images/jyoti.jpg",
       imgAlt: "Dr. Jyoti Chaudhary(HOD)",
       goalName: "Dr. Jyoti Chaudhary",
       goalRole: "HOD",
       goalRole1: "CE Department",
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/anil.jpg",
+      img: "https://images/anil.jpg",
       imgAlt: "Mr. Anil Yadav",
       goalName: "Mr. Anil Yadav",
       goalRole: " Assistant Professor",
       goalRole1: "CE Department",
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/archna.jpg",
+      img: "https://images/archna.jpg",
       imgAlt: "Mrs. Archana parmar",
       goalName: "Mrs. Archana Parmar",
       goalRole: "Assistant Professor",
       goalRole1: "CE Department",
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/ajit.jpg",
+      img: "https://images/ajit.jpg",
       imgAlt: "Mr.Ajit Kadyan",
       goalName: "Mr.Ajit Kadyan",
       goalRole: "Assistant Professor",
       goalRole1: "CE Department",
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/minakshi.jpg",
+      img: "https://images/minakshi.jpg",
       imgAlt: "MS. MEENAKSHI CHAWLA",
       goalName: "MS. Meenakshi Chawla",
       goalRole: "Assistant Professor",
@@ -76,19 +76,19 @@ export function getMentors() {
 export function getDevelopers() {
   return [
     {
-      img: "https://tcs.vidhukant.xyz/images/mohit.jpg",
+      img: "https://images/mohit.jpg",
       imgAlt: "Mohit Saini (Technical Head)",
       title: "Mohit Saini",
       subtitle: "Technical Head",
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/rahul.webp",
+      img: "https://images/rahul.webp",
       imgAlt: "Rahul Goyal(front-end developer)",
       title: "Rahul Goyal",
       subtitle: "Front-End Developer",
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/vidhukant.webp",
+      img: "https://images/vidhukant.webp",
       imgAlt: "Vidhu Kant Sharma(back-end developer)",
       title: "Vidhu Kant sharma",
       subtitle: "Bank-End Developer",
@@ -99,13 +99,13 @@ export function getDevelopers() {
 export function getProjects() {
   return [
     {
-      img: "https://tcs.vidhukant.xyz/images/website.png",
+      img: "https://images/website.png",
       imgAlt: "website",
       goalName: "WEBSITE",
       goalRole: "It describes about the society's event and workshop.",
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/app.png",
+      img: "https://images/app.png",
       imgAlt: "app",
       goalName: "APP",
       goalRole: "It describes about the society's events, members and there mentors information and gallery.",
@@ -116,19 +116,19 @@ export function getProjects() {
 export function getSocietyGoals() {
   return [
     {
-      img: "https://tcs.vidhukant.xyz/images/ppt.jpg",
+      img: "https://images/ppt.jpg",
       imgAlt: "P",
       goalName: "Presentation Expert",
       goalRole: "Presentations are the most important aspect of the IT industry that an intern should be skilled in and we look this skill as an important goal.",
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/resume.webp",
+      img: "https://images/resume.webp",
       imgAlt: "S",
       goalName: "Skilled Resume",
       goalRole: "Making each member aware about the technology prevailing in the IT industry and helping them achieve those skills to fit in resume.",
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/placement.jpeg",
+      img: "https://images/placement.jpeg",
       imgAlt: "P",
       goalName: "Placement Ready",
       goalRole: "The ultimate Goal is to make an individual ready for the Placement that every student thrives for.",
@@ -139,32 +139,32 @@ export function getSocietyGoals() {
 export function getTechnologiesUsed() {
   return [
     {
-      img: "https://tcs.vidhukant.xyz/images/technologies/html.png",
+      img: "https://images/technologies/html.png",
       title: "HTML",
       subtitle: "Use in Web-Development"
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/technologies/css.png",
+      img: "https://images/technologies/css.png",
       title: "CSS",
       subtitle: "Use in Web-Development"
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/technologies/js.png",
+      img: "https://images/technologies/js.png",
       title: "Javascript",
       subtitle: "Use in Web-Development"
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/technologies/react.svg",
+      img: "https:///technologies/react.svg",
       title: "React.js",
       subtitle: "Use in Web-Development"
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/technologies/java.png",
+      img: "https://images/technologies/java.png",
       title: "Java",
       subtitle: "Use in Programing"
     },
     {
-      img: "https://tcs.vidhukant.xyz/images/technologies/cpp.png",
+      img: "https://images/technologies/cpp.png",
       title: "C++",
       subtitle: "Use in Programing"
     },
@@ -177,7 +177,7 @@ export function getFrontpageInfo() {
   return {
     title: "#Let's Get ",
     subtitle: "If future generations are to remember us more with gratitude than sorrow, we must achieve more than just the miracles of technology. We must also leave them a glimpse of the world as it was created, not just as it looked when we got through with it.",
-    logoPath: "https://tcs.vidhukant.xyz/images/site-logo.png",
+    logoPath: "https://images/site-logo.png",
     buttonText: "Click Me!"
   };
 }
@@ -186,29 +186,29 @@ export function getEvents() {
   return [
     {
       title: "BGMI",
-      img: "https://tcs.vidhukant.xyz/images/bgmi.jpeg",
+      img: "https://images/bgmi.jpeg",
       description: "BGMI is the annual gaming event organised by our society in which the winner can redeem the cash prize and t-shirts."
     },
     {
       title: "CODE WAR",
-      img: "https://tcs.vidhukant.xyz/images/codewar.jpeg",
+      img: "https://images/codewar.jpeg",
       description: "Code War is the annual coding event of the society and the winner gets the tag of coder of the year on our website."
     },
     {
       title: "Qates      ",
-      img: "https://tcs.vidhukant.xyz/images/qates.jpeg",
+      img: "https://images/qates.jpeg",
       description: "Qates is the fun-tech Quiz event of the society which is organised to check the tech awareness of the students."
     },
     {
       title: "Slackware",
-      img: "https://tcs.vidhukant.xyz/images/slackware.jpeg",
+      img: "https://images/slackware.jpeg",
       description: "Slackware is an exciting workshop that include making viruses, the remote sensing of different devices and other exciting hacks."
     },
   ];
 }
 
 export function saveRegistration(data, ok, err) {
-    fetch("https://tcs.vidhukant.xyz/api/registrations/recruitment", {
+    fetch("https://api/registrations/recruitment", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
